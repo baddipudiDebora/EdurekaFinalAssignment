145ae7fa63b5f62a63d212ef6fae33b697f8c361
swaroopdebora <kumar11.2k@gmail.com>
2021-01-11 21:45:19 -0500

    added Listeners and Email trigger code

--- src/test/java/com/goibibo/base/BaseClass.java
+++ src/test/java/com/goibibo/base/BaseClass.java
@@ -10,10 +10,37 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeTest;
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
 
 import com.goibibo.pages.*;
 
 public class BaseClass {
+	private static ITestContext context;
 	public static WebDriver driver;
 //	public static Logger log = Logger.getLogger("devpinoyLogger");
 	public static WebDriverWait wait;
@@ -25,10 +52,13 @@ public class BaseClass {
 	public SingleHotelsViewPage singleHotelViewPageobj;
 	public ProceedtoPaymentPage proceedtoPaymentPageobj;
 	public PaymentPage paymentPageobj;
-	
+	public static ITestContext setContext(ITestContext iTestContext, WebDriver driver) {
+		iTestContext.setAttribute("driver", driver);
+		return iTestContext;
+	}
 
-	@BeforeTest
-	public void openBrowser() {
+	@BeforeClass
+	public void openBrowser(ITestContext iTestContext) {
 		if (Constants.browser.equals("chrome")) {
 			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 			Map<String, Object> prefs = new HashMap<String, Object>();
@@ -40,8 +70,9 @@ public class BaseClass {
 			options.addArguments("--disable-extensions");
 			options.addArguments("--disable-infobars");
 			driver = new ChromeDriver(options);
+			context = setContext(iTestContext, driver);
 			}
-		else if (Constants.browser.equals("chrome")) {
+		else if (Constants.browser.equals("ie")) {
 			driver= new InternetExplorerDriver();
 		}
 		driver.get(Constants.testsiteurl);
@@ -59,4 +90,86 @@ public class BaseClass {
 		paymentPageobj = new PaymentPage(driver);
 	}
 
+	@AfterClass()
+	public void closeBrowser() {
+		driver.close();
+	}
+
+	@AfterSuite()
+	public void tearDown() {
+		sendReportByGMail("debora11.2k@gmail.com", "Remarkable@2020", "debora11.2k@gmail.com", "TestCase Report", "");
+		sendReportByGMail("debora11.2k@gmail.com", "Remarkable@2020", "ananyabs2020@gmail.com", "TestCase Report", "");
+		sendReportByGMail("debora11.2k@gmail.com", "Remarkable@2020", "swaroop.22k@gmail.com", "TestCase Report", "");
+
+		}
+
+	/**
+	 * Send email using java
+	 * 
+	 * @param from
+	 * @param pass
+	 * @param to
+	 * @param subject
+	 * @param body
+	 */
+	private static void sendReportByGMail(String from, String pass, String to, String subject, String body) {
+		Properties props = System.getProperties();
+		String host = "smtp.gmail.com";
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.user", from);
+		props.put("mail.smtp.password", pass);
+		props.put("mail.smtp.port", "587");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+		Session session = Session.getDefaultInstance(props);
+		MimeMessage message = new MimeMessage(session);
+
+		try {
+			// Set from address
+			message.setFrom(new InternetAddress(from));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+			// Set subject
+			message.setSubject(subject);
+			message.setText(body);
+
+			BodyPart objMessageBodyPart = new MimeBodyPart();
+
+			objMessageBodyPart.setText("Please Find The Attached Report File!");
+
+			Multipart multipart = new MimeMultipart();
+
+			multipart.addBodyPart(objMessageBodyPart);
+
+			objMessageBodyPart = new MimeBodyPart();
+
+			// Set path to the pdf report file
+			String filename = System.getProperty("C:\\Users\\Deborah\\eclipse-workspace\\FinalAssignment\\test-output\\emailable-report.html");
+			// Create data source to attach the file in mail
+			DataSource source = new FileDataSource(filename);
+
+			objMessageBodyPart.setDataHandler(new DataHandler(source));
+
+			objMessageBodyPart.setFileName(filename);
+
+			multipart.addBodyPart(objMessageBodyPart);
+
+			message.setContent(multipart);
+			Transport transport = session.getTransport("smtp");
+			transport.connect(host, from, pass);
+			transport.sendMessage(message, message.getAllRecipients());
+			transport.close();
+		} catch (AddressException ae) {
+			ae.printStackTrace();
+		} catch (MessagingException me) {
+			me.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
+
